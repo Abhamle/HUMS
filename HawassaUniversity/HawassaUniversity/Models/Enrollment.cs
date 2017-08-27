61565daf36d5cdf518b663f8341148f703d1e5ab
A M <abelmengistu2000@gmail.com>
2017-08-27 17:06:48 -0400

    Creating Student, Course and Enrollment Models

---
+++ HawassaUniversity/HawassaUniversity/Models/Enrollment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HawassaUniversity.Models
+{
+    public enum Grade
+    {
+        A,B,C,D,F
+    }
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        //Foreign Key to Course Entity
+        public int CourseID { get; set; }
+        //Forign key to Student Entity
+        public int StudentID { get; set; }
+        public Grade? Grade { get; set; }
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
